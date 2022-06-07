cb9eb9617c6ac62e59cf18492fc09e85f836f7d3
Gabriel Zuñiga <dukemlet@gmail.com>
2022-06-06 21:56:35 -0500

    Se creo un triangulo

--- Molino/Molino/Source.cpp
+++ Molino/Molino/Source.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <conio.h>
 
+void dibujar(int n) {
+	int aux = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j >= n - i - 1) {
+				aux++;
+				std::cout << aux << " ";
+			}
+			else std::cout << "  ";
+		}
+		for (int j = 1; j < n; j++) {
+			if (j <= i) {
+				aux--;
+				std::cout << aux << " ";
+			}
+			else std::cout << "  ";
+		}
+		std::cout << std::endl;
+		aux = 0;
+	}
+}
+
 void main() {
 
-	
+	int n = 6;
+
+	dibujar(n);
 
 	_getch();
 }
